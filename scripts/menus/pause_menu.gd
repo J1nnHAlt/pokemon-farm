b6479817361008ffc59c1b65f1d09e33d4c8ff7f
jh <jinhaowngaltacc@gmail.com>
2025-07-21 23:19:25 +0800

    pause menu gaodim

---
+++ scripts/menus/pause_menu.gd
@@ -0,0 +1,42 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$AnimationPlayer.play("RESET")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	testEsc()
+
+func resume():
+	get_tree().paused = false
+	$AnimationPlayer.play_backwards("blur")
+
+func pause():
+	get_tree().paused = true
+	$AnimationPlayer.play("blur")
+
+func testEsc():
+	if Input.is_action_just_pressed("esc") and get_tree().paused == false:
+		print("ESC pressed")
+		pause()
+	elif Input.is_action_just_pressed("esc") and get_tree().paused == true:
+		print("ESC pressed")
+		resume()
+
+
+
+func _on_resume_pressed() -> void:
+	resume()
+
+func _on_restart_pressed() -> void:
+	resume()
+	get_tree().reload_current_scene()
+
+func _on_options_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/menus/settings_menu.tscn")
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
