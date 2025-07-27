bb97660b83bbbc6118a002b502d5a27179572ba1
Jade1028 <weijie2828@gmail.com>
2025-07-27 16:43:53 +0800

    Day night cycle done, crops growing on progress

--- scenes/day_night_ui/day_night_cycle_component.gd
+++ scenes/day_night_ui/day_night_cycle_component.gd
@@ -6,7 +6,7 @@ extends CanvasModulate
 		initial_day = id
 		DayAndNightCycleManager.initial_day = id
 		DayAndNightCycleManager.set_initial_time()
-		
+
 @export var initial_hour: int = 12:
 	set(ih):
 		initial_hour = ih
@@ -15,10 +15,10 @@ extends CanvasModulate
 
 @export var initial_minute: int = 30:
 	set(im):
-		initial_hour = im
-		DayAndNightCycleManager.initial_hour = im
+		initial_minute = im
+		DayAndNightCycleManager.initial_minute = im
 		DayAndNightCycleManager.set_initial_time()
-		
+
 @export var day_night_gradient_texture: GradientTexture1D
 
 func _ready() -> void:
@@ -28,7 +28,7 @@ func _ready() -> void:
 	DayAndNightCycleManager.set_initial_time()
 	
 	DayAndNightCycleManager.game_time.connect(on_game_time)
-	
-func on_game_time(time:float) -> void:
-	var sample_value = 0.5 * (sin(time - PI * 0.5) +1.0)
-	color = day_night_gradient_texture.gradient.sample(sample_value)		
+
+func on_game_time(time: float) -> void:
+	var sample_value = sin((time / 1440.0) * TAU - PI / 2.0) * 0.5 + 0.5
+	color = day_night_gradient_texture.gradient.sample(sample_value)
--- scenes/day_night_ui/day_night_panel.gd
+++ scenes/day_night_ui/day_night_panel.gd
@@ -3,9 +3,9 @@ extends Control
 @onready var day_label: Label = $DayPanel/MarginContainer/DayLabel
 @onready var time_label: Label = $TimePanel/MarginContainer/TimeLabel
 
-@export var normal_speed: int = 5
-@export var fast_speed: int = 100
-@export var cheetah_speed: int = 200
+@export var normal_speed: float = 1.0
+@export var fast_speed: float = 5.0
+@export var cheetah_speed: float = 20.0
 
 func _ready() -> void:
 	DayAndNightCycleManager.time_tick.connect(on_time_tick)
