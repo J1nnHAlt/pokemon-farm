b6ee42c43bf3dbc0b03e57c1c7f0f0f4b5030631
jh <jinhaowngaltacc@gmail.com>
2025-08-17 00:09:06 +0800

    entering dungeon level from main scene

---
+++ scenes/test/dungeon_entrance.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+@export var dungeon_scene: String = "res://scenes/levels/dungeon_level/dungeon.tscn"
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_body_entered(body: Node2D) -> void:
+	print("body entered")
+	if body.name == "Player" or body.is_in_group("player"):
+		get_tree().change_scene_to_file(dungeon_scene)
+	pass # Replace with function body.
