070b295d489016b5cbf5cd7468a34fba668c2d14
jh <jinhaowngaltacc@gmail.com>
2025-08-16 21:59:28 +0800

    catching pokemon with pokeballs

--- scenes/characters/player/player.gd
+++ scenes/characters/player/player.gd
@@ -2,6 +2,7 @@ class_name Player
 extends CharacterBody2D
 
 @export var pokeball_scene = preload("res://scenes/pokeball/pokeball.tscn")
+@onready var sfx_throw_pokeball: AudioStreamPlayer = $sfx_throw_pokeball
 
 @export var current_tool: DataTypes.Tools = DataTypes.Tools.None
 var inventory: Inv
@@ -11,6 +12,7 @@ var player_direction: Vector2
 func _ready() -> void:
 	set_inventory()
 	GameData.inventory_loaded.connect(set_inventory)
+
 	
 func _process(delta):
 	if Input.is_action_just_pressed("watering"):
@@ -27,6 +29,7 @@ func set_inventory():
 	inventory = GameData.inventory
 
 func throw_pokeball():
+	sfx_throw_pokeball.play()
 	var pokeball = pokeball_scene.instantiate()
 	get_parent().add_child(pokeball)
 	pokeball.global_position = global_position
--- scenes/pokeball/pokeball.gd
+++ scenes/pokeball/pokeball.gd
@@ -1,11 +1,30 @@
 # Pokeball.gd (attached to your Pokeball scene)
 extends Area2D
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 @export var speed: float = 400.0
 var velocity: Vector2
+var target_pokemon: CharacterBody2D
 
 func _physics_process(delta):
 	position += velocity * delta
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	queue_free()
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("wild_pokemon"):
+#		make it not everytime success
+		animated_sprite_2d.play("hit")
+		velocity = Vector2.ZERO
+		target_pokemon = body
+	else:
+#		for hitting environment
+		queue_free()
+
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	if animated_sprite_2d.animation == "hit":
+		target_pokemon.queue_free()
+		queue_free()
