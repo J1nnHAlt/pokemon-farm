26e7c5472eea261ffc7dfae715f0562414bf80eb
jh <jinhaowngaltacc@gmail.com>
2025-08-16 14:19:14 +0800

    audio for enter and exit

--- scripts/zones/hide_area.gd
+++ scripts/zones/hide_area.gd
@@ -1,7 +1,12 @@
 extends TransformArea
+@onready var sfx_enter: AudioStreamPlayer = $"../../sfx_enter"
+@onready var sfx_exit: AudioStreamPlayer = $"../../sfx_exit"
+
 
 func _transform(_node: Node) -> void:
+	sfx_enter.play()
 	_node.hide()
 
 func _untransform(_node: Node) -> void:
+	sfx_exit.play()
 	_node.show()
