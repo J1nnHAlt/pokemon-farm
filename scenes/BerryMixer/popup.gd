353215efdf978fe7a78bd152cfdf3b25a1c6b354
ztsia <leosia929@gmail.com>
2025-09-20 01:32:04 +0800

    changed asset file directories for berries, petfood

--- scenes/BerryMixer/popup.gd
+++ scenes/BerryMixer/popup.gd
@@ -15,7 +15,7 @@ func set_display(recipe_name: String, msg: String):
 		slot_texture.texture = item.texture
 		name_label.text = item.name
 	else:
-		slot_texture.texture = preload("res://pokemon-assets/Graphics/Items/000.png")
+		slot_texture.texture = preload("res://assets/UI/default_image.png")
 		name_label.text = "Unknown Recipe"
 
 func _on_close_button_pressed():
